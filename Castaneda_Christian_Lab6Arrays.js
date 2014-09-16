9d075086ff6a75fa12f1e1cdd0fcd9c1f3cf342c
Chris Castaneda <SLEVEN15@fullsail.edu>
2014-09-16 13:49:26 -0400

    Third Commit: Array Loop

--- Castaneda_Christian_Lab6Arrays.js
+++ Castaneda_Christian_Lab6Arrays.js
@@ -15,7 +15,15 @@
         var teamCitys = new Array ("New York", "Green Bay", "San Francisco",      "Seattle", "New England",
                                     "Chicago",    "Denver",   "New Orleans", "Philadelphia",       "Dallas");  
                                     
+        for (i = 0; i < footballTeams.length; i++)
+        {
+           console.log("The " + footballTeams[i] + " are a football team in the National Football League and their hometown is " + teamCitys[i] + ".");
+        };    
+        
       
+                                        
+    };
+
         
 
 // MAIN
