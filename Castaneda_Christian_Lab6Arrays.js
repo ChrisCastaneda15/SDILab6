cdcace0c46b7a73dc2eea82af464ecfe102205c8
Chris Castaneda <SLEVEN15@fullsail.edu>
2014-09-16 13:48:09 -0400

    Second Commit: Two arrays

--- Castaneda_Christian_Lab6Arrays.js
+++ Castaneda_Christian_Lab6Arrays.js
@@ -12,7 +12,11 @@
         var footballTeams = new Array ( "Jets", "Packers",    "49ers", "Seahawks", "Patriots",
                                        "Bears", "Broncos",   "Saints",   "Eagles",   "Cowboys");
                                          
-
+        var teamCitys = new Array ("New York", "Green Bay", "San Francisco",      "Seattle", "New England",
+                                    "Chicago",    "Denver",   "New Orleans", "Philadelphia",       "Dallas");  
+                                    
+      
+        
 
 // MAIN
 
