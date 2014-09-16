67474a3d244baeee8908c8701ad32876654b1513
Chris Castaneda <SLEVEN15@fullsail.edu>
2014-09-16 13:45:43 -0400

    First Commit: One array

---
+++ Castaneda_Christian_Lab6Arrays.js
@@ -0,0 +1,20 @@
+/*
+  Christian Castaneda
+  September 16th,2014
+  SDI
+  Lab 6 arrays 
+*/
+
+// Function
+    
+    var array = function()
+    {
+        var footballTeams = new Array ( "Jets", "Packers",    "49ers", "Seahawks", "Patriots",
+                                       "Bears", "Broncos",   "Saints",   "Eagles",   "Cowboys");
+                                         
+
+
+// MAIN
+
+array();
+
